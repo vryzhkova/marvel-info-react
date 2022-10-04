9903b7ec442fdd1c2b0c85cf27072934f49f5f12
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-10-04 14:38:56 +0400

    added random char info

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,22 +2,34 @@ class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=778847f5f231c973c47b211b4be7342d';
   
-        getResource = async(url) => {
-            let res = await fetch(url);
+    getResource = async (url) => {
+        let res = await fetch(url);
   
-        if(!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    if(!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+  
+    return await res.json();
+    }
+  
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+  
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name, 
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
         }
-  
-        return await res.json();
-    }
-  
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-    }
-  
-    getAllCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     }
   }
   
